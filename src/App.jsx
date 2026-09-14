ba331b59f8ee15548482a08065835e3046afdf91
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-09-14 14:32:16 -0400

    feat: render GamePage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
+import { GamePage } from './components/game/GamePage'
 
 function App() {
-  return <></>
+  return (
+    <>
+      <GamePage />
+    </>
+  )
 }
 
 export default App
