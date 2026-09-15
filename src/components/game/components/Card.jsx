40cfa06e92cb1f440d69e04265c0b97ba5ced6de
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-09-15 01:40:20 -0400

    feat: write Card initial jsx (#3)

---
+++ src/components/game/components/Card.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export function Card({cardId, index, imageSrc, suit, value, recordClick}) {
+  const [isFlipped, setFlippedState] = useState(false);
+
+  const handleClick = () => {
+    // Prevents clicking an already flipped card
+    if (isFlipped) return;
+
+    setFlippedState(true);
+    recordClick(cardId);
+  }
+
+  return (
+    <button
+      className={`card${isFlipped ? " flipped" : ""}`}
+      type="button"
+      data-id={cardId}
+      data-suit={suit}
+      data-value={value}
+      aria-label={isFlipped ? `Card #${index + 1}` : `${value} of ${suit}`}
+      onClick={handleClick}
+    >
+      <img src={imageSrc} alt=""></img>
+    </button>
+  )
+}
