039281a129025197bee48e95daae918861c6ed13
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-09-21 14:52:21 -0400

    feat: pass onClick events to Sidebar and assign difficulty as key so gamepage rerenders on diff change. Closes #9

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import './App.css'
+import { useState } from 'react'
 import { GamePage } from './components/game/GamePage'
 import { Sidebar } from './components/sidebar/Sidebar'
+import './App.css'
+
+const DIFFICULTIES = {
+  standard: 30,
+  moderate: 15,
+  extreme: 5,
+}
 
 function App() {
+  const [difficulty, setDifficulty] = useState(DIFFICULTIES.standard);
+
   return (
     <>
-      <Sidebar />
-      <GamePage />
+      <Sidebar 
+        setStandardDiff={() => setDifficulty(DIFFICULTIES.standard)}
+        setModerateDiff={() => setDifficulty(DIFFICULTIES.moderate)}
+        setExtremeDiff={() => setDifficulty(DIFFICULTIES.extreme)}
+      />
+      <GamePage 
+        key={difficulty} // triggers a render if the user changes difficulty
+        difficulty={difficulty}
+      />
     </>
   )
 }
