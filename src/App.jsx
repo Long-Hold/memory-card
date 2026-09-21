8fe5120102faa1b7a851d30aa08fd5c036f850ef
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-09-21 12:56:52 -0400

    feat: render Sidebar component in App (#9)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import { GamePage } from './components/game/GamePage'
+import { Sidebar } from './components/sidebar/Sidebar'
 
 function App() {
   return (
     <>
+      <Sidebar />
       <GamePage />
     </>
   )
