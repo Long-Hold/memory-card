c28d068715f422fc7b4d175cc25c4cac85cb71db
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-09-25 18:48:21 -0400

    feat: autofocus Start Game button with useEffect

--- src/components/game/components/StartScreen.jsx
+++ src/components/game/components/StartScreen.jsx
@@ -1,14 +1,23 @@
-export function StartScreen({loadCards}) {
-  return (
-    <section className="start-screen">
-      <h2>Concentration</h2>
+import { useEffect } from "react"
+import { useRef } from "react"
 
+export function StartScreen({loadCards}) {
+  const buttonRef = useRef(null);
+
+  // Focuses the button on mount, more reliable then autofocus
+  useEffect(() => {
+    buttonRef.current?.focus();
+  }, []);
+
+  return (
+    <div className="start-screen">
       <button
+      ref={buttonRef}
         type="button"
         onClick={loadCards}
       >
         Start Game
       </button>
-    </section>
+    </div>
   )
 }
